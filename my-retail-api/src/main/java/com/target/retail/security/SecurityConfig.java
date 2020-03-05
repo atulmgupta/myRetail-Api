3a7172858c156d20cd72839ce9c66d47de02de03
Atul <iamatulmgupta@gmail.com>
2020-03-05 07:56:04 -0800

    Updated Security Configs to bypass get Methods

--- my-retail-api/src/main/java/com/target/retail/security/SecurityConfig.java
+++ my-retail-api/src/main/java/com/target/retail/security/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.target.retail.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -51,8 +52,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		// We don't need CSRF for this example
 		httpSecurity.csrf().disable()
 				// dont authenticate this particular request
-				.authorizeRequests().antMatchers("/authenticate").permitAll().
-				// all other requests need to be authenticated
+				.authorizeRequests().antMatchers("/authenticate","/v2/api-docs",
+                        "/configuration/ui",
+                        "/swagger-resources/**",
+                        "/configuration/security",
+                        "/swagger-ui.html",
+                        "/webjars/**").permitAll().
+				antMatchers(HttpMethod.GET).permitAll().
 				anyRequest().authenticated().and().
 				// make sure we use stateless session; session won't be used to
 				// store user's state.
